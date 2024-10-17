4ea666c2637042b0646dfaf5bdccc00e2fd74438
Acer <ainapercky@gmail.com>
2024-10-17 08:44:09 +0300

    single page init

--- src/component/AboutSkills.jsx
+++ src/component/AboutSkills.jsx
@@ -7,6 +7,7 @@ export default function AboutSkills() {
         <div>
             <div>
                 <h4 className='mt-3 mb-1 text-2xl'>Skills</h4>
+                {/**/}
                 <div>
                     {
                         dataSkill && dataSkill.map((item)=><ProgressBar class={"bg-red-500"} skill={item.skill} value={item.value}></ProgressBar>)
